c9b6ba21c01ec3d74d488ea0c76c20a2b2def503
Monirul Islam <mmislam027@gmail.com>
2026-05-09 05:41:07 +0600

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    
+    groupAnagrams(strs) {
+        if (!strs || strs.lenght === 0) {
+            return []
+        }
+        const map = new Map()
+
+        strs.forEach(str => {
+            const s = str.split('').sort().join('')
+            if(!map.has(s)) map.set(s , [])
+            map.get(s).push(str)
+            
+        })
+        return Array.from(map.values())
+    }
+}
